ea684734c428ea7743220bf628930e017694cf29
Blake Krupa <Blake.Krupa@gmail.com>
2022-03-16 13:05:25 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -49,7 +49,7 @@ df = df.groupby(["Founded Year"]).count().reset_index()
 
 st.markdown('From the Chart below we notice an increase in Unicorn Companies around the mid 2010s with a steep decline following 2015')
 
-col5, col6 = st.columns((1,1))
+#col5, col6 = st.columns((1,1))
 
 
 
@@ -59,9 +59,9 @@ chart = alt.Chart(df).mark_bar().encode(
     alt.X('Founded Year:T', title = 'Year Founded'),
     alt.Y('Company', title = 'Total Count')
 ).properties(
-    width=600  # controls width of bar.
+    width=800  # controls width of bar.
 )
-with col5:
-    st.altair_chart(chart, use_container_width=True)
-with col6:
-    st.dataframe(df) 
+#with col5:
+st.altair_chart(chart, use_container_width=True)
+#with col6:
+st.dataframe(df) 
