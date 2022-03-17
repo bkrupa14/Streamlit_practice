092a6e9a78a6a4a0cfa1e7e581be459bc69c8b6a
Blake Krupa <Blake.Krupa@gmail.com>
2022-03-17 13:29:34 -0600

    update

--- streamlit_data.py
+++ streamlit_data.py
@@ -4,31 +4,30 @@ import numpy as np
 
 #df = pd.read_csv('/Users/blakekrupa/Desktop/School/Python/Unicorn_Companies.csv')
 df = pd.read_csv('Unicorn_Companies.csv')
-print(df.columns)
 df = df[['Company','Founded Year', 'Industry' ]]
 df = df.replace(to_replace ="None",
+                 value =np.nan)
+df = df.replace(to_replace ="Other",
                  value =np.nan)
 df = df.dropna()
 
 
 
-df['Company'] = df['Company'].astype(str)
+
+df['Industry'] = df['Industry'].astype(str)
 df[['Founded Year']] = df[['Founded Year']].apply(pd.to_numeric) 
 
 
 
 df = df[(df['Founded Year'] > 1999 ) &
-          (df['Founded Year'] <2022  ) ]
+          (df['Founded Year'] <2022  )]
 
 df = df.groupby(["Founded Year",'Industry']).count().reset_index()
-
+df = df[(df['Company'] >3 )]
 # %%
 df
 # %%
-import matplotlib.pyplot as plt
-df.plot.bar(x="Founded Year", y="Company", title="Number of Unicorn Companies founded by Year");
 
-plt.show()
 # %%
 df.columns
 # %%
@@ -40,7 +39,13 @@ chart_data
 df.dtypes
 # %%
 
-df['Founded Year'] = df['Founded Year'].dt.year
+import altair as alt
+chart = alt.Chart(df).mark_bar().encode(
+    alt.X('Founded Year:O', title = 'Year Founded'),
+    alt.Y('Company', title = 'Total Count'),
+    color='Industry')
+
+chart
+# %%
 df
 # %%
-df['Founded Year'] = pd.to_datetime(df['Founded Year'])
